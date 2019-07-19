e532bb71e7ef04702c810ab68f7ab0ce26171f24
bryan <bryanprz00@gmail.com>
2019-07-18 20:44:04 -0700

    Use npm to build on heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::API
   def set_studio
     @studio = Studio.find(params[:id])
   end
+
+  def fallback_index_html
+    render :file => 'public/index.html'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
