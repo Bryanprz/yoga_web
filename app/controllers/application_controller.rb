c1eaddd0baa8421aaf77796cc1163b16184a878e
bryan <bryanprz00@gmail.com>
2019-07-20 12:56:37 -0700

    Route debuggin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,8 @@ class ApplicationController < ActionController::API
 
   def fallback_index_html
   	respond_to do |format|
-  	  format.html { render body: Rails.root.join('public/index.html').read }
+  	  # format.html { render body: Rails.root.join('public/index.html').read }
+  	  format.html { render file: "public/index.html" }
   	end
   end
 end
