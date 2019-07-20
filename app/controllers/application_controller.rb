63cb3575ae4735a6e95baa6f4335a787d905a9ca
bryan <bryanprz00@gmail.com>
2019-07-20 13:01:21 -0700

    routes fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,7 @@ class ApplicationController < ActionController::API
 
   def fallback_index_html
   	respond_to do |format|
-  	  # format.html { render body: Rails.root.join('public/index.html').read }
-  	  format.html { render file: "public/index.html" }
+  	  format.html { render body: Rails.root.join('public/index.html').read }
   	end
   end
 end
